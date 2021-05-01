54672d58d5f8bd7326cbdf610ee86d8a811439b5
Marcos Campos <mvocampos@yahoo.com.br>
2021-05-01 15:08:06 -0300

    project started

---
+++ app/src/main/java/com/example/appmarvel/ui/details/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appmarvel.ui.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appmarvel.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.details_activity)
+    }
+}
---
+++ app/src/main/java/com/example/appmarvel/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appmarvel.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appmarvel.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
