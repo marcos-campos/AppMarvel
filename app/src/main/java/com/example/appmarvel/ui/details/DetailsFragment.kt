10cf4a4daf8217e17e4f939bee029b94d61d3821
Marcos Campos <mvocampos@yahoo.com.br>
2021-05-03 18:37:42 -0300

    button action has been added

--- app/src/main/java/com/example/appmarvel/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/appmarvel/ui/details/DetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.appmarvel.ui.details
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import com.example.appmarvel.R
 import com.example.appmarvel.model.Result
 import com.example.appmarvel.ui.clickImage.ClickImageActivity
 import com.squareup.picasso.Picasso
+import java.time.LocalDate
 
 
 class DetailsFragment(private val comicsInfo: Result) : Fragment() {
